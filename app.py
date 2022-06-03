009d798daf6043df4e28ac10e8fc32f0a47aed79
JamieClipperton <babygatter@yahoo.com>
2022-06-02 19:12:24 -0500

    update

--- app.py
+++ app.py
@@ -34,8 +34,11 @@ def home():
         f"All available routes:<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
     )
 
+#Percipitations
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     last_year_data = dt. date(2017,8,23) - dt.timedelta(days=365)
@@ -44,3 +47,44 @@ def precipitation():
         order_by(Measurement.date).all()
     prcp_val_list = dict(prcp_values)
     return jsonify(prcp_val_list)
+
+#Stations
+@app.route("/api/v1.0/stations")
+def stations():
+    stat_list = session.query(Station.station, Station.name).all()
+    all_stat = list(stat_list)
+    return jsonify(all_stat)
+
+#TOBS Route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    last_year_data = dt. date(2017,8,23) - dt.timedelta(days=365)
+    tobs_value = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= last_year_data).\
+        order_by(Measurement.date).all()
+    tobs_lst = list(tobs_value)
+    return jsonify(tobs_lst)
+
+
+#Start Day Route
+@app.route("/api/v1.0/<start>")
+def start_day(start):
+    start_day = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        group_by(Measurement.date).all()
+    start_list = list(start_day)
+    return jsonify(start_list)
+
+#Start-End Day Route
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    start_end = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).\
+        group_by(Measurement.date).all()
+    start_end_lst = list(start_end)
+    return jsonify(start_end_lst)
+
+#Define Behavior
+if __name__ == '_main_':
+    app.run(debug=True)
